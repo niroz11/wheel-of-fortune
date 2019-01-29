a68e547949fbf909632e2847d07a9c2fce0ccb81
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-01-29 14:46:16 -0700

    adding game time starter kit

---
+++ src/player.js
@@ -0,0 +1,8 @@
+class Player{
+	constructor(name){
+		this.name = name;
+		this.account = 0;
+		this.bank = 0;
+	}
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+  // CSS and file (image) loaders
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'images/',
+                  publicPath: 'images/'
+                }
+              }
+            ]
+      }
+    ],
+  },
+  // Below is needed for webpack-dev-server
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+ 		contentBase: './dist'
+  }
+};
