4975ff7036bad84b8dd06396fd7d5e386f4ecb77
Niraj Aryal <niroz.aryal11@gmail.com>
2019-01-30 09:19:18 -0700

    stash error

--- src/game.js
+++ src/game.js
@@ -1,8 +1,24 @@
 class Game{
 	constructor(){
+		this.player = []
+	}
+	createPlayers(){
+		let player1 = new Player('Frank');
+		let player2 = new Player ('Gabe');
+		let player3 = new Player('Adam');
+		this.player.push(new player("Frank"))
 		
+
 	}
-	addBoard(){
-		$('.board-piece').text('hello world');
-	}
-}
+
+
+};
+
+
+
+
+
+
+
+
+
