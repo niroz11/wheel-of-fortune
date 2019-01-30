039bc7b7331c1e449a26b1b6c2fd965362f06d63
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-01-29 19:10:16 -0700

    basic of wheel created

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,22 @@
 class DomUpdates{
-	constructor(){
-		
+	displayWheel(){
+		let div = $(
+			`<section class="wheel-section">
+				<h2>player one</h2>
+				<div class="wheel">
+					<p>hello</p>
+				</div>
+				<button class="
+				spin-button"></button>
+			</section>
+			`
+		)
+		$("body").append(div);
+	}
+	spinAnimation(num){
+		$(".wheel").css("transform", `rotate(${num}deg)`);
+	}
+	removeWheel(){
+		$(".wheel-section").remove();
 	}
 }
--- src/wheel.js
+++ src/wheel.js
@@ -1,5 +1,20 @@
 class Wheel{
 	constructor(){
-		
+		this.wheelValue = 0;
+		this.PLayervalue = 0;
+	}
+	SetWheelValue(){
+
+	}
+	spinWheel(){
+		let wheelValue = Math.floor(Math.random() * 6) + 1;
+		let wheelSpin =  wheelValue * (Math.floor(Math.random() * 6) + 360);
+		domUpdates.spinAnimation(wheelSpin)
+		setTimeout(() => {
+			domUpdates.removeWheel()
+		}, 10000)
+	}
+	addValueToPlayer(){
+		domUpdates.addScore();
 	}
 }
