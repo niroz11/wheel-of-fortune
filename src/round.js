c9f5ea5c21c85495c675bf2c128ada67b4b0602f
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-02-01 11:18:30 -0700

    parse the phrase data to grab four random phrases from easy to hardest

--- src/round.js
+++ src/round.js
@@ -1,8 +1,21 @@
 class Round{
     constructor(data){
-        this.roundData;
+        this.gameData = data;
+        this.gameQuestions;
     }
     setupround(){
         wheel.SetWheelValue()
+        this.parseDataForGame()
+    }
+    parseDataForGame(){
+        this.gameQuestions = Object.keys(this.gameData).reduce((acc, round) => {
+            let array = this.gameData[round].puzzle_bank
+                for (let i =  array.length - 1; i > 0; i--) {
+                    const r = Math.floor(Math.random() * (i + 1));
+                    [array[i], array[r]] = [array[r], array[i]];
+                } 
+            acc.push(array.pop());
+            return acc;
+        }, [])
     }
 }
