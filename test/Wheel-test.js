ba6fb63a4bf6d8d37919a12c342221bf20fd50fe
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-02-08 16:47:08 -0700

    remove duplicates

--- test/Wheel-test.js
+++ test/Wheel-test.js
@@ -35,43 +35,25 @@ describe('Wheel', function() {
 		wheel.SetWheelValue();
 		expect(wheel.currentWheelValue).to.have.length(6);
 	})
-	it("should be able to spin the wheel and set a value", () => {
-		wheel.currentWheelValue = [1,2,3,4,5,6]
-		wheel.spinWheel()
-		expect(wheel.spinValue).to.be.an("number")
-	})
 	it("should display the enter letter if player guess right", () => {
 		wheel.determinePlayer()
 		expect(domUpdates.displayEnterLetter).to.have.been.called()
 	})
 
-it('it should set wheel value', function(){
-	wheel.SetWheelValue();
-	expect(wheel.currentWheelValue).to.have.length(6);
-
-})
-
-it('it should spin the wheel', function(){
-	wheel.SetWheelValue();
-	wheel.spinWheel();
-	expect(domUpdates.spinAnimation).to.have.been.called(1);
-	// expect(domUpdates.displayWheelValue).to.have.been.called(1);
-	// expect(domUpdates.removeWheel).to.have.been.called(1);
-	 expect(wheel.spinValue).that.is.a('number');
-})
-
-it('it should invoke updateScore when determinePlayer', function(){
-	//setup
-	let game = new Game(data);
-	let player = new Player();
-	wheel.spinValue = "BANKRUPT";
-	game.player = [{name:'bob',account:0, bank:0},{name:'niraj',account:0, bank:0},{name:'justin',account:0, bank:0},]
-	//execution
-	wheel.determinePlayer(game);
-	//expectation
-	expect(domUpdates.updateScore).to.have.been.called(1);
-
-})
+	it('should spin the wheel', function(){
+		wheel.SetWheelValue();
+		wheel.spinWheel();
+		expect(domUpdates.spinAnimation).to.have.been.called(1);
+		expect(wheel.spinValue).that.is.a('number');
+	})
+	it('should invoke updateScore when determinePlayer', function(){
+		let game = new Game(data);
+		let player = new Player();
+		wheel.spinValue = "BANKRUPT";
+		game.player = [{name:'bob',account:0, bank:0},{name:'niraj',account:0, bank:0},{name:'justin',account:0, bank:0},]
+		wheel.determinePlayer(game);
+		expect(domUpdates.updateScore).to.have.been.called(1);
+	})
 
 });
 
